add08dfdfb49d24a8a02abf8ae04c8ba738e043f
umutsahinturk <umut.sahinturk@std.yildiz.edu.tr>
2025-01-12 18:19:33 +0300

    enum yapıları oluşturuldu

--- main.c
+++ main.c
@@ -2,6 +2,36 @@
 
 int main()
 {
-    
+    enum AccelRange
+    {
+        _2G,
+        _4G,
+        _8G,
+        _16G
+    };
+
+    enum Bandwidth
+    {
+        _7_81Hz,
+        _15_63Hz,
+        _31_25Hz,
+        _62_5Hz,
+        _125Hz,
+        _250Hz,
+        _500Hz,
+        _1000Hz,
+    };
+
+    enum OperationMode 
+    {
+        Normal,
+        Suspend,
+        LowPower1,
+        Standby,
+        LowPower2,
+        DeepSuspend
+    };
+
+
     return 0;
 }
